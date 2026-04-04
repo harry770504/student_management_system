2f47810071c898d9335d8c029db1d1acdf8e0745
Priyanshu Kushwaha <priyanshukushwaha145@gmail.com>
2026-04-04 23:42:22 +0530

    added marks feature

--- student/models.py
+++ student/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from teacher.models import Teacher
 
 class Student(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -11,6 +12,28 @@ class Student(models.Model):
 
     def __str__(self):
         return self.full_name
+    
+class Subject(models.Model):
+    subject_name = models.CharField(max_length=100, unique=True)
+    subject_code = models.CharField(max_length=100,unique=True)
+
+    def __str__(self):
+        return self.subject_name    
 
 
+class Marks(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+
+    exam_type = models.CharField(max_length=50)   # Midterm, Final, Unit Test
+    marks_obtained = models.DecimalField(max_digits=5, decimal_places=2)
+    total_marks = models.DecimalField(max_digits=5, decimal_places=2, default=100)
+
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.student.full_name} - {self.subject.subject_name} - {self.marks_obtained}"
 # Create your models here.
+
+
